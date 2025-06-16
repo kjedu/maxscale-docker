5c554407451c8794ad3422b2389583e8b4592888
Kaylie Velazquez-Becerra <kjvelazquezbecerra@student.rtc.edu>
2025-06-15 20:25:44 -0700

    Create main.py
    Python script that contains: the largest zipcode in zipcodes_one, all zipcodes where state=KY (Kentucky), all zipcodes between 40000 and 41000, the TotalWages column where state=PA (Pennsylvania) along with name, email, date, class and a brief description on what the code does.

---
+++ maxscale/main.py
@@ -0,0 +1,36 @@
+# Kaylie Velazquez
+# kjvelazquezbecerra@student.rtc.edu
+# 05/29/2025
+# Spring Qrt
+# CNE 370: Introduction to Virtualization
+
+# The following script will connect to Maxscale Instance and will be able to execute queries to display the following:
+# Largest zipcodes in zipcode_one, all zipcodes in Kentucky, zipcodes between 40000-41000 and the TotalWage where state=PA
+
+import mysql.connector
+
+# connection to Maxscale connection
+def connect_to_sql():
+    conn = mysql.connector.connect(host='192.168.1.43', port=4000, user= "maxuser", password="maxpwd")
+
+    return conn
+
+con = connect_to_sql()
+cursor = con.cursor()
+
+# The largest zipcode in zipcode_one
+cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 1;")
+print(cursor.fetchall())
+
+# All zipcode where state=KY (Kentucky)
+cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one WHERE State='KY';")
+print(cursor.fetchall())
+
+# All zipcodes between 4000 and 41000
+cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one WHERE zipcode BETWEEN 40000 and 41000;")
+print(cursor.fetchall())
+
+# The TotalWages column where state=PA (Pennsylvania)
+cursor.execute("SELECT TotalWages FROM zipcodes_one.zipcodes_one WHERE state= 'PA';")
+print(cursor.fetchall())
+
